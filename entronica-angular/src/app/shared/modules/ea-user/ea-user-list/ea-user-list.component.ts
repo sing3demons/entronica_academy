be4dfae563446a17a3b8361ba91330972f06c305
sing3demons <sing3demons@live.com>
2022-09-26 15:42:54 +0700

    refactor: move user-list to ea-user-list

--- entronica-angular/src/app/shared/modules/ea-user/ea-user-list/ea-user-list.component.ts
+++ entronica-angular/src/app/shared/modules/ea-user/ea-user-list/ea-user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core'
+import { User } from 'src/app/model/user.model'
+import { UserService } from 'src/app/services/user.service'
 
 @Component({
   selector: 'ea-user-list',
@@ -6,7 +8,13 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./ea-user-list.component.css'],
 })
 export class EaUserListComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+  constructor(private userService: UserService) {}
+  users: User[] = []
+  selectedUser: User | undefined
+  ngOnInit(): void {
+    this.users = this.userService.getAllUsers()
+  }
+  showDetail(user: User): void {
+    this.selectedUser = user
+  }
 }
