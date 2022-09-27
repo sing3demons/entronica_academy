c6f98754394df87c2868f263717a030f46ccb514
sing3demons <sing3demons@live.com>
2022-09-27 11:01:54 +0700

    feat: create componet ea-user-detail, move div user-detail to component ea-user-detail

--- entronica-angular/src/app/shared/modules/ea-user/ea-user-detail/ea-user-detail.component.ts
+++ entronica-angular/src/app/shared/modules/ea-user/ea-user-detail/ea-user-detail.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
+import { User } from 'src/app/shared/model/user.model'
 
 @Component({
-  selector: 'app-ea-user-detail',
+  selector: 'ea-user-detail',
   templateUrl: './ea-user-detail.component.html',
-  styleUrls: ['./ea-user-detail.component.css']
+  styleUrls: ['./ea-user-detail.component.css'],
 })
 export class EaUserDetailComponent implements OnInit {
+  @Input()
+  selectedUser: User | undefined
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
