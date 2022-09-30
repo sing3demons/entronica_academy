5b8dd586bbf3513d16a218898ffcccbaab7418b4
sing3demons <sing3demons@live.com>
2022-09-30 09:29:30 +0700

    init project

--- test_full_stack_work-shop/work-shop-notebook/src/app/app.module.ts
+++ test_full_stack_work-shop/work-shop-notebook/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateAssetComponent } from './components/form/create-asset/create-asset.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, CreateAssetComponent],
+  declarations: [AppComponent, CreateAssetComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
